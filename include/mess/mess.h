bbd746bfe98ba32e8b539e4bd71b572f561657db
Louis-Charles Caron <louis-charles.caron@sensefly.com>
2020-04-15 21:35:42 +0200

    Oh well...

--- include/mess/mess.h
+++ include/mess/mess.h
@@ -67,7 +67,7 @@ namespace mess
 	template<auto P>
 	struct IsPointedToBy: WithNoArgument
 	{
-		static_assert(std::is_pointer_v<decltype(P)>, "Template argument must be a pointer.");
+		static_assert(!std::is_void_v<decltype(*P)>, "Template argument must be a allow dereferencing (e.i. *P is well formed).");
 		static constexpr auto pointer = P;
 		using Nature = ::mess::impl::Pointer;
 	};
